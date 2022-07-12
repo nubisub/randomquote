ae8ddfc846f2d85a309cd5d635d5de5f3dc25018
nubisub <hendraksm18@mail.ugm.ac.id>
2022-07-12 20:42:10 +0700

    finale

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import fetch from 'node-fetch';
 
 import './App.css';
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import {Button, Container, CssBaseline} from "@mui/material";
+import {Button, Container} from "@mui/material";
 import TwitterIcon from '@mui/icons-material/Twitter';
 import RedditIcon from '@mui/icons-material/Reddit';
 import {useEffect, useState} from "react";
@@ -12,8 +11,8 @@ import {useEffect, useState} from "react";
 function Quote(props){
     return (
         <>
-            <p className="text-3xl mx-0">{props.quote.quote}</p>
-            <p className="text-xl text-center sm:text-right my-4 font-light " >- {props.quote.author}</p>
+            <p id="text" className="text-3xl mx-0">{props.quote.quote}</p>
+            <p id="author" className="text-xl text-center sm:text-right my-4 font-light " >- {props.quote.author}</p>
         </>
     )
 }
@@ -54,7 +53,7 @@ function App(){
                     response.json())
             .then(
                 response =>  {
-                    if(response.message == "You have exceeded the rate limit per second for your plan, BASIC, by the API provider"){
+                    if(response.message === "You have exceeded the rate limit per second for your plan, BASIC, by the API provider"){
                         let updatedValue = {
                             quote : "Too Many Request :( ",
                             author : "Hirohito"
@@ -85,19 +84,19 @@ function App(){
 
 
     return(
-        <div className="App h-screen flex items-center p-8" style={{backgroundColor : color}} >
+        <div id="quote-box" className="App h-screen flex items-center p-8" style={{backgroundColor : color}} >
             <Container maxWidth="sm" className="">
                 <Box className="p-12 " sx={{ bgcolor: '#cfe8fc' }} >
-        <Quote quote={quote}/>
-    <div className="sm:grid sm:grid-cols-2 sm:mt-8" >
-            <div className="flex sm:justify-start justify-left">
-                <Button style={{backgroundColor : color}}  sx={{width: {xs: 1/2, sm:"auto"},}} variant="contained" ><TwitterIcon/></Button>
-                <Button style={{backgroundColor : color}}  sx={{ml: 1, width: {xs: 1/2, sm:"auto"}}}  variant="contained" ><RedditIcon/></Button>
-            </div>
-            <div className="flex sm:justify-end justify-left sm:mt-0 mt-2">
-                <Button style={{backgroundColor : color}}  onClick={() => setCount((c) => c + 1)}  sx={{textTransform: 'none',  width: {xs: 1, sm:"auto"}}} variant="contained" >New Quote</Button>
-            </div>
-        </div>
+                    <Quote quote={quote}/>
+                    <div className="sm:grid sm:grid-cols-2 sm:mt-8" >
+                        <div className="flex sm:justify-start justify-left">
+                            <Button id="tweet-quote" style={{backgroundColor : color}}  sx={{padding:0, width: {xs: 1/2, sm:"auto"},}} variant="contained" ><a target="_blank" className="h-full w-full flex items-center justify-center" href={"https://twitter.com/intent/tweet?text="+ quote.quote + " - "+ quote.author}><TwitterIcon/></a></Button>
+                            <Button style={{backgroundColor : color}}  sx={{ml: 1, width: {xs: 1/2, sm:"auto"}}}  variant="contained" ><a className="h-full w-full flex items-center justify-center" href="#"><RedditIcon/></a></Button>
+                        </div>
+                        <div className="flex sm:justify-end justify-left sm:mt-0 mt-2">
+                            <Button id="new-quote" style={{backgroundColor : color}}  onClick={() => setCount((c) => c + 1)}  sx={{textTransform: 'none',  width: {xs: 1, sm:"auto"}}} variant="contained" >New Quote</Button>
+                        </div>
+                    </div>
                 </Box>
             </Container>
         </div>
